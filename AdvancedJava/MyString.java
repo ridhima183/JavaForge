bad33d12fac04ea52ce6c0a57e41ba1e358b93d3
Ridhima Gadalay <ridhima.gadalay@gmail.com>
2026-07-16 16:46:07 +0530

    Add files via upload

---
+++ AdvancedJava/MyString.java
@@ -0,0 +1,13 @@
+package newfeatures;
+/**
+*Author : Bhavna
+*Date : 14-Jul-2026
+*Time :10:33:37 am
+*email :ridhima.gadalay@gmail.com
+*/
+public interface MyString {
+	
+	//Abstract by default 
+	String myStringFunction(String str);
+
+}
