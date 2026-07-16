0e008d1802a5f0d8b2c080f26d36775a49596d23
Ridhima Gadalay <ridhima.gadalay@gmail.com>
2026-07-16 16:41:21 +0530

    Add files via upload

---
+++ AdvancedJava/Calculator.java
@@ -0,0 +1,14 @@
+package newfeatures;
+/**
+*Author : Bhavna
+*Date : 14-Jul-2026
+*Time :10:17:07 am
+*email :ridhima.gadalay@gmail.com
+*/
+@FunctionalInterface
+public interface Calculator {
+	
+	int calculate(int a, int b);
+
+}
+
