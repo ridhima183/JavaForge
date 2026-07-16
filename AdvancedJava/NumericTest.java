b9427bd32f98e2dc7be68fad767129c037895688
Ridhima Gadalay <ridhima.gadalay@gmail.com>
2026-07-16 16:46:48 +0530

    Add files via upload

---
+++ AdvancedJava/NumericTest.java
@@ -0,0 +1,14 @@
+package newfeatures;
+/**
+*Author : Bhavna
+*Date : 14-Jul-2026
+*Time :10:51:21 am
+*email :ridhima.gadalay@gmail.com
+*/
+public interface NumericTest {
+	
+	boolean ComputeTest(int n); // by default public abstract 
+	
+	
+
+}
