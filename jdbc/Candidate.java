da4ccee60468010e0af95a4d03ac1d16f989092e
Ridhima Gadalay <ridhima.gadalay@gmail.com>
2026-07-21 18:53:23 +0530

    Add files via upload

---
+++ jdbc/Candidate.java
@@ -0,0 +1,90 @@
+package jdbcdemo;
+
+/**
+ * Author : sirin
+ * Date : Jul 13, 2026
+ * Time : 2:44:25 PM
+ * Email : sirinandini.a@gmail.com
+ */
+
+public class Candidate {
+
+	private String firstName;
+	private String lastName;
+	private String dob;
+	private String phone;
+	private String email;
+	
+	
+	public Candidate() {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.phone = phone;
+		this.email = email;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the dob
+	 */
+	public String getDob() {
+		return dob;
+	}
+	/**
+	 * @param dob the dob to set
+	 */
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+}
