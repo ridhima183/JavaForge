52e0dc5f9da179d9580a3f8d573f68a4b4ea2ccd
Ridhima Gadalay <ridhima.gadalay@gmail.com>
2026-07-31 09:56:52 +0530

    Add files via upload

---
+++ day5/StringOperation.java
@@ -0,0 +1,16 @@
+package assignments.day6;
+/**
+*Author : Bhavna
+*Date : 16-Jul-2026
+*Time :10:36:01 pm
+*email :ridhima.gadalay@gmail.com
+*/
+	
+	@FunctionalInterface
+	public interface StringOperation {
+
+	    // Functional Interface Method
+	    String operate(String str);
+
+	}
+
