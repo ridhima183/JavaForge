007d5d0502e8de4eebd9f9497f5d1cb1e7eee0f3
Ridhima Gadalay <ridhima.gadalay@gmail.com>
2026-07-31 09:55:34 +0530

    Add files via upload

---
+++ day5/PerformOperation.java
@@ -0,0 +1,12 @@
+package assignments.day6;
+/**
+*Author : Bhavna
+*Date : 16-Jul-2026
+*Time :10:23:51 pm
+*email :ridhima.gadalay@gmail.com
+*/
+	
+interface PerformOperation {
+	    boolean check(int n);
+	}
+
